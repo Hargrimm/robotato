e6447a2413ada0d99b62ebc0b11f671c6d7b9ccf
John Boyle <boylejm@gmail.com>
2013-03-30 01:15:02 -0700

    added youtube code back to robotato.pl and removed 'require _youtube.pl' since something ain't working right

--- robotato.pl
+++ robotato.pl
@@ -6,11 +6,14 @@ use Irssi;
 use warnings;
 use vars qw($VERSION %IRSSI);
 use Data::Dumper;
+use WWW::Mechanize;
+use URI::Find::Schemeless;
+
 our $robodir = "/home/johnstein/.irssi/scripts/";
 
 require $robodir . '_weather.pl';
 require $robodir . '_git.pl';
-require $robodir . '_youtube.pl';
+#require $robodir . '_youtube.pl';
 require $robodir . '_googme.pl';
 
 $VERSION = '2.00';
@@ -197,6 +200,50 @@ sub priv_dispatch {
     return unless (grep {lc($_) eq lc($nick)} keys %commands);
     $ts->send_message($target_chan[0], $msg, 0);
 }
+
+
+sub scrapeYT {
+    my ($msg,  $target_chan) = @_;
+    my @uris;
+    my $finder = URI::Find::Schemeless->new(sub {
+        my $uri = shift;
+        push @uris, $uri;
+    });
+    my $numurl = $finder->find(\$msg);
+    my $uri = $uris[0];
+
+    $uri->scheme("http");
+    
+    if (head $uri ) {
+        my $mech = WWW::Mechanize->new();
+        $mech->get( $uri ) or die();
+        my $title = $mech->title();
+        $title =~ s/- YouTube$//;
+        printf "%s\n", $title;
+        public_msg($target_chan, "YouTube: " . $title);
+    }
+    else {
+        printf "BAD YOUTUBE LINK!";
+        public_msg($target_chan, "CONFUSING YOUTUBE URL!! :C :C");
+    }
+}
+
+sub checkYT {
+    my $url = shift;
+    my $yt1 = "www.youtube.com";
+    my $yt2 = "http://youtu.be/";
+
+    if (index($url,$yt1) != -1) {
+        return 1;
+    }
+    elsif (index($url,$yt2) != -1) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 Irssi::signal_add("message public", "dispatch");
 Irssi::signal_add("message private", "priv_dispatch");
 #Irssi::settings_add_str("targetchan", "target_chan", "");
